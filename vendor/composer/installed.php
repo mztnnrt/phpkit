6ca118e16054dedf44b9d66aa03da5c3b1ea24ad
mztnnrt <mizutani_norito@cam-inc.co.jp>
2021-03-17 21:02:56 +0900

    fix name

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,19 +6,19 @@
     'aliases' => 
     array (
     ),
-    'reference' => '4efb3df66c0e1f657d6fe8522bf1e01ab02b1306',
-    'name' => 's01638/phpkit',
+    'reference' => 'c7b5540528501756d792462a268e3fdc55621223',
+    'name' => 'mztnnrt/phpkit',
   ),
   'versions' => 
   array (
-    's01638/phpkit' => 
+    'mztnnrt/phpkit' => 
     array (
       'pretty_version' => '1.x-dev',
       'version' => '1.9999999.9999999.9999999-dev',
       'aliases' => 
       array (
       ),
-      'reference' => '4efb3df66c0e1f657d6fe8522bf1e01ab02b1306',
+      'reference' => 'c7b5540528501756d792462a268e3fdc55621223',
     ),
   ),
 );
